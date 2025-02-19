570f6682449c06b31886767a24ea3cd8b3cde140
bekashantadze <bekashantadze10@gmail.com>
2025-02-19 16:43:25 +0400

    second commit

---
+++ day 04/homework/hw.py
@@ -0,0 +1,17 @@
+mothername="dodo"
+fathername="omari"
+brothername2="suxo"
+brothername="nodar"
+sistername="diana"
+sistername2="ana"
+print(mothername)
+print(fathername)
+print(brothername)
+print(brothername2)
+print(sistername)
+print(sistername2)
+
+
+
+
+
