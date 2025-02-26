ebb767794740c3cbe567bf221488530621e9f3d6
bekashantadze <bekashantadze10@gmail.com>
2025-02-26 21:57:13 +0400

    second commit

---
+++ day 06/homework/hw.py
@@ -0,0 +1,6 @@
+# მომხმარებელს შემოატანინეთ თავისი სახელი, გვარი, ასაკი და ქალაქი. შემდეგ ეს ინფორმაცია ერთ წინადადებად დაუბეჭდეთ ტერმინალში.
+name="beqa"
+surname="shantadze"
+age=15
+vilige="khulo"
+print( " my name " + name + surname + " my age "+ str (age) + " chemi sacxovrebeli adgilia " + vilige)
---
+++ day 06/homework/hw1.py
@@ -0,0 +1,11 @@
+# შევქმნათ ცვლადი სახელად celsius
+celsius= 30
+fahrenheit=(celsius* 9/5) + 32
+kelvin = celsius + 273.1
+# შევქმენით ცვლადი celsius
+# შემდეგ შევქმენით ცვლადი fahrenheit და დავუმატეთ მეორე ცვლადი celsius,ასევე დავუმატეთ 32
+# შემდეგ შევქმენით კიდევ ახალი ცვლადი kelvin სადაც შევინახეთ და ეს იქნება celsius ცვლადში.
+
+print(celsius)
+print(fahrenheit)
+print(kelvin)
