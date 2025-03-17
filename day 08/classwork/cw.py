c031ae5b962a212fe99efcf2d49ce1ecbd5ed272
bekashantadze <bekashantadze10@gmail.com>
2025-03-17 20:32:34 +0400

    second commit

--- day 08/classwork/cw.py
+++ day 08/classwork/cw.py
@@ -1,4 +1,4 @@
 # შექმენით პროგრამა, რომელშიც მომხმარებელს შემოატანინებთ სახელსა და გვარს, შემდეგ კი დაუბეჭდავთ ერთად.
 name=input( "whats your name: ")
 surname=input( "whats your surname: ")
-print("hay" + " " + name + surname)
+print("hello" + " " + name + surname)
