356319f90504183e55b35376c22772f31f17c841
bekashantadze <bekashantadze10@gmail.com>
2025-05-08 21:47:32 +0400

    second commit

---
+++ day 26/homework/hw1.py
@@ -0,0 +1,16 @@
+
+basket = ["ვაშლი", "ბანანი", "ფორთოხალი", "ატამი", "ქლიავი"]
+
+favorite_fruit = input("შეიყვანეთ თქვენი საყვარელი ხილი: ")
+
+fruit_in_basket = False
+
+for fruit in basket:
+    if fruit == favorite_fruit:
+        fruit_in_basket = True
+        break
+
+if fruit_in_basket:
+    print("Good choice")
+else:
+    print("Fruit not in basket")
---
+++ day 26/homework/hw3.py
@@ -0,0 +1,11 @@
+# 5) შექმენით პროგრამა, რომელიც მოხმარებელს მოსთხოვს წინდადების შეყვანას, შემდეგ კი ამ წინადადებას სამჯერ დაბეჭდავს სხვადასხვა სახით: lower, upper, capitalize ფორმატებში
+sentence = input("შეიყვანეთ წინადადება: ")
+
+
+print("Lower:", sentence.lower())
+
+
+print("Upper:", sentence.upper())
+
+
+print("Capitalized:", sentence.capitalize())
